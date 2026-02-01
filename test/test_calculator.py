2079de60abe3ff16fe01f437bc4765b3a81c6943
Arjun SIngh <arjun.jagdishsaran1997@gmail.com>
2026-02-01 10:13:48 +0530

    Update test_calculator.py

--- test/test_calculator.py
+++ test/test_calculator.py
@@ -1,7 +1,13 @@
-from app.calculator import add, subtract
+from app.calculator import add,substract
 
+'''
+  from app -> folder
+  calculator -> fileName
+  import add,substract (functions)
+'''
 def test_add():
-    assert add(2, 3) == 5
+  assert add(2,3) == 5
 
-def test_subtract():
-    assert subtract(5, 3) == 2
+
+def test_substract():
+  assert substract(5,3) == 2
